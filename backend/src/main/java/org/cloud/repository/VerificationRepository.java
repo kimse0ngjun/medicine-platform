c765116353ffdb007939aecb734c73b7da0eb54f
kimse0ngjun <tjdwns8756@naver.com>
2026-05-08 18:09:35 +0900

    cleanup: backend 폴더 제거

--- backend/src/main/java/org/cloud/repository/VerificationRepository.java
+++
@@ -1,7 +0,0 @@
-package org.cloud.repository;
-
-import org.cloud.entity.Verification;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VerificationRepository extends JpaRepository<Verification, Long> {
-}
